b997e6eae71f89b5c50a2f37121a258cf679a9b6
kwinkunks <matt@agilegeoscience.com>
2015-03-01 15:47:41 -0400

    reorg into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='striplog',
+      version='0.1',
+      description='Tools for making and managing well data.',
+      url='http://github.com/agile-geoscience/striplog',
+      author='Agile Geoscience',
+      author_email='hello@agilegeoscience.com',
+      license='Apache 2',
+      packages=['striplog'],
+      zip_safe=False)
