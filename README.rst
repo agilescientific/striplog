3256c488495263cac386a5061eca5a6530033b48
kwinkunks <matt@agilegeoscience.com>
2015-03-21 13:52:32 -0300

    successful pypi

---
+++ README.rst
@@ -0,0 +1,26 @@
+striplog
+========
+
+.. image:: https://pypip.in/status/striplog/badge.svg
+    :target: https://pypi.python.org/pypi/striplog/
+    :alt: Development status
+
+.. image:: https://www.codacy.com/project/badge/ad9af103cba14d33abd5b327727ff644 
+    :target: https://www.codacy.com/public/matt/striplog
+    :alt: Code review
+
+.. image:: https://pypip.in/license/striplog/badge.svg
+    :target: https://pypi.python.org/pypi/striplog/
+    :alt: License
+
+Lithology and stratigraphic logs for wells. 
+
+Utilities for handling lithologic and stratigraphic 'interval' logs. Includes a version of `las.py` for loading LAS well log data. 
+
+Dependencies
+------------
+
+* `NumPy <http://www.numpy.org/>_`
+* `Pillow <https://pillow.readthedocs.org/>_`
+* `matplotlib <http://matplotlib.org/>_`
+
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+REQUIREMENTS = ['numpy', 'pillow', 'matplotlib']
+CLASSIFIERS = ['Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+]
 
 setup(name='striplog',
-      version='0.4',
+      version='0.4.1',
       description='Tools for making and managing well data.',
       url='http://github.com/agile-geoscience/striplog',
       author='Agile Geoscience',
       author_email='hello@agilegeoscience.com',
       license='Apache 2',
       packages=['striplog'],
+      download_url='https://github.com/agile-geoscience/tarball/0.4.1',
       install_requires=REQUIREMENTS,
-      zip_safe=False)
+      classifiers=CLASSIFIERS,
+      zip_safe=False,
+)
