ad30db2bdc6e97b65df1c92cb25c0f3d909787fb
kwinkunks <matt@agilegeoscience.com>
2015-06-15 15:23:23 -0300

    version update

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf 8 -*-
 """
 Python installation file.
@@ -15,18 +16,20 @@ CLASSIFIERS = ['Development Status :: 4 - Beta',
                'License :: OSI Approved :: Apache Software License',
                'Operating System :: OS Independent',
                'Programming Language :: Python',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.4',
                ]
 
 setup(name='striplog',
-      version='0.4.2',
+      version='0.5.0',
       description='Tools for making and managing well data.',
       url='http://github.com/agile-geoscience/striplog',
       author='Agile Geoscience',
       author_email='hello@agilegeoscience.com',
       license='Apache 2',
       packages=['striplog'],
-      download_url='https://github.com/agile-geoscience/tarball/0.4.2',
+      download_url='https://github.com/agile-geoscience/tarball/0.5.0',
       install_requires=REQUIREMENTS,
       classifiers=CLASSIFIERS,
       zip_safe=False,
-)
+      )
