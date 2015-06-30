3f795a7f6ba7220449ba4e7f18d36c75da5fa139
kwinkunks <matt@agilegeoscience.com>
2015-06-30 20:43:57 -0300

    tests attempt

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf 8 -*-
+"""
+Define a suite a tests for the utils module.
+"""
+
+from striplog.utils import rgb_to_hex
+
+
+def test_tohex():
+    black = rgb_to_hex([0, 0, 0])
+    white = rgb_to_hex([255, 255, 255])
+    pink = rgb_to_hex([255, 128, 128])
+
+    assert black == '#000000'
+    assert white == '#FFFFFF'
+    assert pink == '#FF8080'
