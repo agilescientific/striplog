d65dfc489dfb01ba72cd8d9d88e296775e772cd6
kwinkunks <matt@agilegeoscience.com>
2016-03-28 15:45:58 -0300

    3.6 not allowed in pypi yet

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ CLASSIFIERS = ['Development Status :: 4 - Beta',
                'Programming Language :: Python :: 3.3',
                'Programming Language :: Python :: 3.4',
                'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6',
                ]
 
 setup(name='striplog',
